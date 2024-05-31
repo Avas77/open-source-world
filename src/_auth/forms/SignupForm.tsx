b3d83b0f5431f0ac331a6442aa29da324877eb3e
Avas <avas.bajracharya@renegadeinsurance.com>
2024-05-31 16:58:27 +0545

    feat: added rhf and name input in form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignupValidationSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const SignupForm = () => {
-  return <div>SignOutForm</div>;
+  const form = useForm<z.infer<typeof SignupValidationSchema>>({
+    resolver: zodResolver(SignupValidationSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SignupValidationSchema>) => {};
+
+  return (
+    <section className="flex-center flex-col sm:w-420">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </section>
+  );
 };
 
 export default SignupForm;
